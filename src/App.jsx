b5b500e489ef05c1d7182e96d682c7ea2b49d9d3
xDamneDx <xdamnedx@gmail.com>
2022-05-31 11:37:09 +0300

    Added markup for displaying animals

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import "./App.css";
+// Components:
+import Animal from "./Animal";
+
+// Hooks:
+import { useAnimalSearch } from "./hooks/useAnimalSearch";
 
 function App() {
-  return <div className="App"></div>;
+  const { search, animals } = useAnimalSearch();
+
+  return (
+    <main>
+      <h1>Animal Farm</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => search(e.target.value)}
+      />
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+        {animals.length === 0 && "No animals found."}
+      </ul>
+    </main>
+  );
 }
 
 export default App;
